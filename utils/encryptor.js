8e8f7d96a76be78adc307047520afa1934444680
meivannangit <meivannanjayalakshim@gmail.com>
2025-06-18 18:35:10 +0530

    payment logic

--- utils/encryptor.js
+++ utils/encryptor.js
@@ -26,5 +26,39 @@ function decrypt(encText, workingKey) {
   decrypted += decipher.final('utf8');
   return decrypted;
 }
+function getAlgorithm(keyBase64) {
+    var key = Buffer.from(keyBase64, 'base64');
+    switch (key.length) {
+        case 16:
+            return 'aes-128-cbc';
+        case 32:
+            return 'aes-256-cbc';
 
-module.exports = { encrypt, decrypt };
+    }
+    throw new Error('Invalid key length: ' + key.length);
+}
+
+function encryptcc(plainText, keyBase64, ivBase64) {
+
+    const key = Buffer.from(keyBase64, 'base64');
+    const iv = Buffer.from(ivBase64, 'base64');
+
+    const cipher = crypto.createCipheriv(getAlgorithm(keyBase64), key, iv);
+    let encrypted = cipher.update(plainText, 'utf8', 'hex')
+    encrypted += cipher.final('hex');
+    return encrypted;
+}
+
+function decryptcc(messagebase64, keyBase64, ivBase64) {
+
+    const key = Buffer.from(keyBase64, 'base64');
+    const iv = Buffer.from(ivBase64, 'base64');
+
+    const decipher = crypto.createDecipheriv(getAlgorithm(keyBase64), key, iv);
+    let decrypted = decipher.update(messagebase64, 'hex');
+    decrypted += decipher.final();
+    return decrypted;
+}
+
+
+module.exports = { encrypt, decrypt,decryptcc,encryptcc };
