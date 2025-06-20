3447dfb86ea0dc334670476246af4a2e531c23fa
meivannangit <meivannanjayalakshim@gmail.com>
2025-06-20 20:21:13 +0530

    Payment logic redirect url

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -264,15 +264,17 @@ console.log("paymentData",JSON.stringify(paymentData))
 },
  handleCCAvenueResponse: (encResp) => {
     try {
-      const workingKey = process.env.RESPONSE_WORKING_KEY || 'YOUR_WORKING_KEY';
+      const workingKey = process.env.REQUEST_WORKING_KEY || 'YOUR_WORKING_KEY';
       console.log("response working key",workingKey)
       //Generate Md5 hash for the key and then convert in base64 string
       var md5 = crypto.createHash('md5').update(workingKey).digest();
+      console.log("MD5 Key Length:", md5.length)
       var keyBase64 = Buffer.from(md5).toString('base64');
        var ivBase64 = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,0x0e, 0x0f]).toString('base64');
       //const decrypted = decrypt(encResp, workingKey);
-      console.log("encResp",encResp)
+     
        const decrypted=decryptcc(encResp, keyBase64, ivBase64)
+       console.log("decrypated",decrypted)
       const paymentData = qs.parse(decrypted);
       console.log("payment data ",paymentData)
      
